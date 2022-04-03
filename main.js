0678115049400be60695b975b248f04936e9a5ae
rajakiru <raja.kiruthika@yahoo.com>
2022-04-03 16:41:37 +0900

    Added navigation bar and menu

---
+++ main.js
@@ -0,0 +1,8 @@
+//JavaScipt for Navigation menu
+function openNav() {
+  document.getElementById("mySidenav").style.width = "180px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
