d6ce197cafc58a15d575c0e2047b07a1eeae3ba9
rajakiru <raja.kiruthika@yahoo.com>
2022-04-08 21:13:43 +0900

    function to delete, add and edit recipes added

--- model/recipe_db.php
+++ model/recipe_db.php
@@ -37,4 +37,48 @@ function get_recipe($recipe_id) {
     return $recipe;
 }
 
+function delete_recipe($recipe_id) {
+  global $db;
+  $query = 'DELETE FROM recipe_details
+            WHERE recipeID = :recipe_id';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':recipe_id', $recipe_id);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
+function add_recipe($category_id, $cuisine_id, $name, $ingredients, $procedure, $source) {
+global $db;
+$query = 'INSERT INTO recipe_details
+             (categoryID, cuisineID, recipeTitle, recipeIngredients, recipeProcedure, recipeSource)
+          VALUES
+             (:category_id, :cuisine_id,:name, :ingredients, :procedure, :source)';
+$statement = $db->prepare($query);
+$statement->bindValue(':category_id', $category_id);
+$statement->bindValue(':cuisine_id', $cuisine_id);
+$statement->bindValue(':name', $name);
+$statement->bindValue(':ingredients', $ingredients);
+$statement->bindValue(':procedure', $procedure);
+$statement->bindValue(':source', $source);
+$statement->execute();
+$statement->closeCursor();
+}
+
+function edit_recipe($recipe_id, $category_id, $cuisine_id, $name, $ingredients, $procedure, $source) {
+global $db;
+$query = 'UPDATE recipe_details
+          SET categoryID= :category_id, cuisineID= :cuisine_id, recipeTitle= :name, recipeIngredients =:ingredients, recipeProcedure =:procedure, recipeSource =:source
+          WHERE recipeID = :recipe_id';
+$statement = $db->prepare($query);
+$statement->bindValue(':recipe_id', $recipe_id);
+$statement->bindValue(':category_id', $category_id);
+$statement->bindValue(':cuisine_id', $cuisine_id);
+$statement->bindValue(':name', $name);
+$statement->bindValue(':ingredients', $ingredients);
+$statement->bindValue(':procedure', $procedure);
+$statement->bindValue(':source', $source);
+$statement->execute();
+$statement->closeCursor();
+}
+
 ?>
